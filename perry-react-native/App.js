c33ee3ac61f8744990741035b73cd4ca5e4e2d82
Claudio Spiess <claudiosv@users.noreply.github.com>
2018-09-22 13:24:08 -0400

    Enhance react native app

--- perry-react-native/App.js
+++ perry-react-native/App.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import {
+  DrawerLayoutAndroid,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  Dimensions
+} from "react-native";
 import Map from "./Map";
 import { Location, Permissions } from "expo";
-import YelpService from "./perry";
+import ApiService from "./perry";
+import { Constants } from "expo";
+import { List, ListItem, Header } from "react-native-elements"; // 0.16.0
+import "@expo/vector-icons"; // 5.2.0
 
 const deltas = {
   latitudeDelta: 0.0922,
@@ -18,20 +28,38 @@ const region = {
 };
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    //this.openDrawer = this.openDrawer.bind(this);
+  }
+
+  openDrawer = () => {
+    this.drawer.openDrawer();
+  };
+
   state = {
+    devices: [],
     region: null,
     coffeeShops: []
   };
 
-  componentWillMount() {
+  componentDidMount() {
     this.getLocationAsync();
+    this.getDevices();
   }
 
-  getCoffeeShops = async () => {
+  getDevicePath = async deviceName => {
     const { latitude, longitude } = this.state.region;
     const userLocation = { latitude, longitude };
-    const coffeeShops = await YelpService.getCoffeeShops(userLocation);
+    const coffeeShops = await ApiService.getDevicePath(deviceName);
     this.setState({ coffeeShops });
+  };
+
+  getDevices = async () => {
+    const devices = await ApiService.getDeviceList(this.getDevicePath);
+    console.log("dev: ", devices);
+    this.setState({ devices });
+    console.log("State: ", this.state);
   };
 
   getLocationAsync = async () => {
@@ -41,30 +69,73 @@ export default class App extends React.Component {
         errorMessage: "Permission to access location was denied"
       });
     }
-    let location = await Location.getCurrentPositionAsync().catch((r) => console.log(r));
+    let location = await Location.getCurrentPositionAsync().catch(r =>
+      console.log(r)
+    );
     const region = {
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
       ...deltas
     };
     await this.setState({ region });
-    await this.getCoffeeShops();
+    //  await this.getDevicePath();
+    //await this.getDevices();
   };
 
   render() {
+    let devices = this.state.devices.map(device => (
+      <ListItem
+        key={device.name}
+        title={device.name}
+        leftIcon={{ name: "devices" }}
+        onPress={() => {
+          this.getDevicePath(device.name);
+        }}
+      />
+    ));
+    let navigationView = (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#fff",
+          paddingTop: Constants.statusBarHeight
+        }}
+      >
+        <List>{devices}</List>
+      </View>
+    );
+    //
+
+    /*
+    onPress: {this.refs["DRAWER"].openDrawer()}
+    */
     return (
-      <SafeAreaView style={styles.container}>
-        <Map region={region} places={this.state.coffeeShops} />
-      </SafeAreaView>
+      <DrawerLayoutAndroid
+        drawerWidth={200}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => navigationView}
+        ref={_drawer => (this.drawer = _drawer)}
+        style={styles.container}
+      >
+        <Header
+          leftComponent={{
+            icon: "devices",
+            color: "#fff",
+            onPress: this.openDrawer
+          }}
+          centerComponent={{ text: "Perry", style: { color: "#fff" } }}
+          backgroundColor="teal"
+        />
+        <Map
+          style={{ height: 50 }}
+          region={region}
+          places={this.state.coffeeShops}
+        />
+      </DrawerLayoutAndroid>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+  container: {}
 });
--- perry-react-native/perry.js
+++ perry-react-native/perry.js
@@ -1,28 +1,43 @@
-import axios from 'axios'
-
+import axios from "axios";
+import { ListItem } from "react-native-elements";
+import React from "react";
 
 const api = axios.create({
-  baseURL: 'http://192.168.86.241:8080/device',
+  baseURL: "http://192.168.86.241:8080/",
   headers: {
-    Token: 'jnjrineifnajen',
-  },
-})
+    Token: "jnjrineifnajen"
+  }
+});
 
-const getCoffeeShops = userLocation => {
+const getDevicePath = deviceName => {
   return api
-    .get('/arduino-vespa/path')
+    .get(`device/${deviceName}/path`)
     .then(res =>
       res.data.map(path => {
         return {
           name: path.date_added,
           coords: [path.latitude, path.longitude],
-          children: path.device_id,
-        }
+          children: path.device_id
+        };
       })
     )
-    .catch(error => console.error(error))
-}
+    .catch(error => console.error(error));
+};
+
+const getDeviceList = pathFunction => {
+  return api
+    .get("devices")
+    .then(res =>
+      res.data.map(device => {
+        return {
+          name: device.topic
+        };
+      })
+    )
+    .catch(error => console.error(error));
+};
 
 export default {
-  getCoffeeShops,
-}
+  getDeviceList,
+  getDevicePath
+};
